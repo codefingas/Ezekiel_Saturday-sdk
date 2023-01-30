595d81be3b5f649704476234d0b6d38ca57354f5
codefingas <udohezekiel34@yahoo.com>
2023-01-30 07:41:39 +0100

    feature: added jest for tests

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+    testEnvironment: 'node',
+    roots: ['./src'],
+    testMatch: ['**/__tests__/**/*.+(ts|js)', '**/?(*.)+(spec|test).+(ts|js)'],
+    transform: {
+        '^.+\\.(js|ts)$': 'babel-jest',
+    },
+    moduleDirectories: ['node_modules', 'src'],
+    collectCoverage: true,
+    collectCoverageFrom: ['**/*.{js,ts}', '!**/*.d.ts'],
+}
