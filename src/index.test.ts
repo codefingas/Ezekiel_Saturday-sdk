bc8aa84d6a1b2df12dee74b74bdb2d8aba8c65ca
codefingas <udohezekiel34@yahoo.com>
2023-01-30 20:03:58 +0100

    update  & feature: updated test cases; and confirms if we can get comments

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,16 +1,20 @@
 import TheOneApi from "./index";
-import { get } from "./connector/agent";
 
 jest.mock("./index.ts");
 jest.mocked("./connector/agent.ts");
 
-const mockedGet = get as jest.MockedFunction<typeof get>;
 const MockedTheOneApi = TheOneApi as jest.MockedFunctionDeep<typeof TheOneApi>;
 const mockGetMovies = jest.fn();
+const mockGetOneMovie = jest.fn(x => x);
+const mockGetMovieQuotes = jest.fn(x => x)
 
 jest.mock("./index.ts", () => {
     return jest.fn().mockImplementation(() => {
-        return { getMovies: mockGetMovies };
+        return { 
+            getMovies: mockGetMovies,
+            getOneMovie: mockGetOneMovie,
+            getMovieQuotes: mockGetMovieQuotes
+         };
     });
 });
 
@@ -27,5 +31,22 @@ describe("SDK utilization", () => {
         const spy = jest.spyOn(theOneApi, "getMovies");
         theOneApi.getMovies();
         expect(spy).toHaveBeenCalled();
-    })
+    });
+
+    it("retrieves a single movie", () => {
+        const theOneApi = TheOneApi({ accessToken: "secret-access-key" });
+        const spy = jest.spyOn(theOneApi, "getOneMovie");
+        theOneApi.getOneMovie("specific-movie-id");
+        expect(spy).toHaveBeenCalled();
+        expect(spy).toHaveBeenCalledWith("specific-movie-id");
+    });
+
+
+    it("retrieves a movie's quotes", () => {
+        const theOneApi = TheOneApi({ accessToken: "secret-access-key" });
+        const spy = jest.spyOn(theOneApi, "getMovieQuotes");
+        theOneApi.getMovieQuotes("specific-movie-id");
+        expect(spy).toHaveBeenCalled();
+        expect(spy).toHaveBeenCalledWith("specific-movie-id");
+    });
 });
